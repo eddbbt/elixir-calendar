44d2940afcfcaa7be156c7ecdcd6b6b74ef590ba
Eddy Babetto <eddy.babetto@gmail.com>
2023-10-18 17:10:04 +0200

    edited .gitignore

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,8 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :calendar_test, CalendarTest.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "eddy",
+  password: "eddy",
   hostname: "localhost",
   database: "calendar_test_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
